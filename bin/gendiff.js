998eb94ef5905254aad154c0e0bdd11e81cdd9df
kendudan <annadudek19920509@gmail.com>
2021-12-09 15:12:40 +0200

    добавлен функционал для сравнения вложенных файлов

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,42 +3,47 @@ import path from 'path';
 import process from 'process';
 import _ from 'lodash';
 import getParsedData from '../src/parsers.js';
+import stylish from '../src/formatter.js';
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const file1Extension = path.extname(filepath1);
   const file2Extension = path.extname(filepath2);
 
   const data1 = fs.readFileSync(path.resolve(process.cwd(), filepath1), 'utf-8');
   const parsedData1 = getParsedData(data1, file1Extension);
-  const keys1 = Object.keys(parsedData1);
-  const entries1 = Object.entries(parsedData1);
 
   const data2 = fs.readFileSync(path.resolve(process.cwd(), filepath2), 'utf-8');
   const parsedData2 = getParsedData(data2, file2Extension);
-  const keys2 = Object.keys(parsedData2);
-  const entries2 = Object.entries(parsedData2);
 
-  const result = [];
-  entries1.forEach(([key1, value1]) => {
-    if (!keys2.includes(key1)) {
-      result.push(`- ${key1}: ${value1}`);
-    }
-    entries2.forEach(([key2, value2]) => {
-      if (_.indexOf(keys1, key2) === -1) {
-        if (!result.includes(`+ ${key2}: ${value2}`)) {
-          result.push(`+ ${key2}: ${value2}`);
-        }
+  const getData = (currValue1, currValue2) => {
+    const keys1 = Object.keys(currValue1);
+    const keys2 = Object.keys(currValue2);
+    const allKeys = keys1.concat(keys2);
+    const uniqueKeys = _.uniq(allKeys);
+    return uniqueKeys.map((key) => {
+      if (_.isEqual(currValue1[key], currValue2[key])) {
+        return { key, type: 'common', value: currValue1[key] };
       }
-      if (key1 === key2 && value1 === value2) {
-        result.push(`  ${key1}: ${value1}`);
+      if (_.has(currValue1, key) && !_.has(currValue2, key)) {
+        return { key, type: 'onlyFirst', value: currValue1[key] };
       }
-      if (key1 === key2 && value1 !== value2) {
-        result.push(`- ${key1}: ${value1}`);
-        result.push(`+ ${key1}: ${value2}`);
+      if (!_.has(currValue1, key) && _.has(currValue2, key)) {
+        return { key, type: 'onlySecond', value: currValue2[key] };
       }
+      if (_.isObject(currValue1[key]) && _.isObject(currValue2[key])) {
+        const children = getData(currValue1[key], currValue2[key]);
+        return { key, type: 'nested', children };
+      }
+      return {
+        key, type: 'various', firstValue: currValue1[key], secondValue: currValue2[key],
+      };
     });
-  });
-  return `{\n  ${result.sort((a, b) => a.slice(2, 7).localeCompare(b.slice(2, 7))).join('\n  ')}\n}`;
+  };
+  const data = getData(parsedData1, parsedData2);
+  if (format === 'stylish') {
+    return stylish(data, 2);
+  }
+  return 'Unknown format';
 };
 
 export default genDiff;
---
+++ src/formatter.js
@@ -0,0 +1,64 @@
+import _ from 'lodash';
+
+const stylish = (currentValue, depth) => {
+  const replacer = ' ';
+  const currentIndent = replacer.repeat(depth);
+  const bracketIndent = replacer.repeat(depth - 2);
+  const sortedCurrentValue = _.sortBy(currentValue, (o) => o.key);
+
+  if (_.isPlainObject(currentValue)) {
+    const entries = Object.entries(currentValue).map(([key, value]) => {
+      if (typeof value === 'object') {
+        return `${currentIndent}  ${key}: ${stylish(value, depth + 4)}`;
+      }
+      return `${currentIndent}  ${key}: ${value}`;
+    });
+    return [
+      '{',
+      ...entries,
+      `${bracketIndent}}`,
+    ].join('\n');
+  }
+
+  const lines = sortedCurrentValue.map((el) => {
+    if (el.type === 'nested') {
+      return `${currentIndent}  ${el.key}: ${stylish(el.children, depth + 4)}`;
+    }
+    if (el.type === 'various') {
+      if (typeof el.firstValue === 'object') {
+        return `${currentIndent}- ${el.key}: ${stylish(el.firstValue, depth + 4)}\n${currentIndent}+ ${el.key}: ${el.secondValue}`;
+      }
+      if (typeof el.secondValue === 'object' && el.secondValue !== null) {
+        return `${currentIndent}- ${el.key}: ${el.firstValue}\n
+        ${currentIndent}+ ${el.key}: ${stylish(el.secondValue, depth + 1)}`;
+      }
+      return `${currentIndent}- ${el.key}: ${el.firstValue}\n${currentIndent}+ ${el.key}: ${el.secondValue}`;
+    }
+    if (el.type === 'common') {
+      return `${currentIndent}  ${el.key}: ${el.value}`;
+    }
+    if (el.type === 'onlyFirst') {
+      if (typeof el.value === 'object') {
+        return `${currentIndent}- ${el.key}: ${stylish(el.value, depth + 4)}`;
+      }
+      return `${currentIndent}- ${el.key}: ${el.value}`;
+    }
+    if (el.type === 'onlySecond') {
+      if (typeof el.value === 'object') {
+        return `${currentIndent}+ ${el.key}: ${stylish(el.value, depth + 4)}`;
+      }
+      return `${currentIndent}+ ${el.key}: ${el.value}`;
+    }
+    if (typeof el.value === 'object') {
+      return `${currentIndent}${el.key}: ${stylish(el.value, depth + 4)}`;
+    }
+    return `${currentIndent}${el.key}: ${el.value}`;
+  });
+  return [
+    '{',
+    ...lines,
+    `${bracketIndent}}`,
+  ].join('\n');
+};
+
+export default stylish;
