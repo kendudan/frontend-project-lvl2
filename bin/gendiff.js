0b253318d38cb5e3212c51b888b9aa3d30bed112
kendudan <annadudek19920509@gmail.com>
2022-01-19 12:40:02 +0200

    исправлен импорт

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import program from 'commander';
 import genDiff from '../index.js';
-
-const program = new Command();
 
 program
   .version('0.0.1')
